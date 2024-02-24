047c6ed8dac782f3f170c0680fc992359e6c18fe
YangWithU <plzw8@outlook.com>
2024-02-24 16:12:32 +0000

    init

--- chap1/main.cpp
+++ chap1/main.cpp
@@ -1,5 +1,7 @@
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 
 int main() {
-  std::cout << "sdf\n";
+  std::cout << std::fixed << std::setprecision(7) << 3 / std::pow(9, 6) << '\n';
 }
